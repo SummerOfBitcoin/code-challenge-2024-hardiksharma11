3f4b870867c7704f7102d93af59b1273cba27a61
Hardik Sharma <hardik182002@gmail.com>
2024-03-30 23:41:18 +0530

    block version 3

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ def serialize_block_header(block):
 # Function to mine the block
 def mine_block(merkle_root):
     block = {
-        "version": 1,
+        "version": 3,
         "previous_block_hash": "0000000000000000000000000000000000000000000000000000000000000000",
         "merkle_root": merkle_root,
         "timestamp": int(time.time()),
