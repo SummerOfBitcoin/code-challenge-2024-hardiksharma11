e6e12f9c2ecf5a4e8ec90a1ec3179de8e246ea2d
Hardik Sharma <hardik182002@gmail.com>
2024-04-15 11:53:33 +0530

    hit n try

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     transactions = read_transactions()
     transactions = serialize_transactions(transactions)
     transactions = validate_transaction(transactions)
-    transactions = transactions[0:5]
+    transactions = transactions[0:4]
 
     fees = 0
     for transaction in transactions:
