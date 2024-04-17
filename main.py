70344b29c1292b95715a91d4f118055b0df2b57c
Hardik Sharma <hardik182002@gmail.com>
2024-04-17 13:24:19 +0530

    try

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     transactions = read_transactions()
     transactions = serialize_transactions(transactions)
     transactions = validate_transaction(transactions)
-    transactions = transactions[10:14]
+    transactions = transactions[10:12]
 
     print(json.dumps(transactions, indent=4))
 
