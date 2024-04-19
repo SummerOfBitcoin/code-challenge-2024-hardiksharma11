204d66853d3e798382059060d2de6924328f91ca
Hardik Sharma <hardik182002@gmail.com>
2024-04-19 10:08:07 +0530

    try

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
     transactions = serialize_transactions(transactions)
     transactions = validate_transaction(transactions)
     # transactions = sorted(transactions, key=get_fee)
-    transactions = transactions[0:2000]
+    transactions = transactions[0:20]
 
     # print(json.dumps(transactions,indent=4))
 
